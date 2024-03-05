87630e544b9920748f35d729e5b88c37cf26d8ae
Jean Silva <jeanbarrossilva@outlook.com>
2024-03-05 09:33:16 -0300

    refact: centralize generator creation

--- src/generation/generator.c
+++ src/generation/generator.c
@@ -7,18 +7,22 @@
 
 #include "generator.h"
 
-void use_generator(Continuation *continuation, Generator *next, Usage usage) {
+// Creates a generator.
+static Generator *create_generator(Continuation *continuation, Generator *next) {
   Generator *generator = malloc(sizeof(Generator));
   generator->continuation = continuation;
   generator->next = (struct Generator *) next;
+  return generator;
+}
+
+void use_generator(Continuation *continuation, Generator *next, Usage usage) {
+  Generator *generator = create_generator(continuation, next);
   usage(generator);
   free(generator);
 }
 
 void generate(Continuation continuation, Generator *next) {
-  Generator *current = malloc(sizeof(Generator));
-  current->continuation = continuation;
-  current->next = (struct Generator *) next;
+  Generator *current = create_generator(continuation, next);
   void *yielded = NULL;
   do {
     yielded = current->continuation(yielded);
